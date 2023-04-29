cd0a8440fa5d44bf0e8ab681077219687bbaa217
Eshita Islam <islam.eshita@gmail.com>
2023-04-29 11:44:59 -0700

    Added support for clear

---
+++ src/components/ToDo.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, clearTodo, removeAt } from "../features/todoSlice";
+
+function Todo() {
+  const items = useSelector((state) => state.todo.items);
+  const dispatch = useDispatch();
+  const [input, setInput] = useState("");
+  const inputText = useRef();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+    // Clear the input box
+    inputText.current.value = "";
+  };
+
+  return (
+    <div>
+      <hr />
+      <h2>To do List</h2>
+      <hr />
+      <form onSubmit={(e) => submitForm(e)}>
+        <input
+          ref={inputText}
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Add to List</button>
+      </form>
+      <button onClick={() => dispatch(clearTodo())}>Clear</button>
+      <ul>
+        {items.map((todoItem, index) => {
+          return <li key={index}>{todoItem}</li>;
+        })}
+      </ul>
+      <hr />
+    </div>
+  );
+}
+
+export default Todo;
